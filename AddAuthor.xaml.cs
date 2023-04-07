e5818d61c14ffde710735550458843b00311e7fb
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-04-07 19:42:13 +0200

    some type of layout

--- AddAuthor.xaml.cs
+++ AddAuthor.xaml.cs
@@ -23,5 +23,10 @@ namespace Library
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
