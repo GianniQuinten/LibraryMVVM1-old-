81adb36d17eae281b01b710e27a1a41da5f379bb
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-04-09 11:23:49 +0200

    made all models public. i forgot to do that

--- Models/ItemAuthor.cs
+++ Models/ItemAuthor.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Library.Models
 {
-    class ItemAuthor
+    public class ItemAuthor
     {
         public int AuthorId { get; set; }
         public Author Author { get; set; }
