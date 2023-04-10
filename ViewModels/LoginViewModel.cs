9f4354281c60c510bc5d492b829298f6a6a71fee
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2023-04-10 21:58:40 +0200

    Creating a login

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Library.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        // Fields 
+        private string _email;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible=true;
+
+        public string Email { 
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+        public SecureString Password { 
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage { 
+            get => _errorMessage;
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible { 
+            get => _isViewVisible;
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        // Commands
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        // Constructor
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p => ExecuteRecoverPassCommand("",""));
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrWhiteSpace(Email) || Email.Length < 3 || Password == null || Password.Length < 3)
+                validData = false;
+            else
+                validData = true;
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        { 
+            throw new NotImplementedException(); 
+        }
+
+        private void ExecuteRecoverPassCommand(string name, string email)
+        { 
+            throw new NotImplementedException(); 
+        }
+    }
+}
