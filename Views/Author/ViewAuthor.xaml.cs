7a68c4871fd8e7eac230529ba1f17fbc15d42863
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-04-14 12:13:19 +0200

    working on the datagrid cell

--- Views/Author/ViewAuthor.xaml.cs
+++ Views/Author/ViewAuthor.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +26,22 @@ namespace Library
             InitializeComponent();
         }
 
+        private void ViewAuthor_Load(object sender, EventArgs e)
+        {
+            EditPanel.Visibility = Visibility.Collapsed;
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=Library;";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            cmd.CommandText = "SELECT * FROM AUTHORS";
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            DataGrid.DataContext = ds.Tables[0];
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
@@ -37,6 +55,29 @@ namespace Library
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_CellClick(object sender, SelectionChangedEventArgs e)
+        {
+            //if (datagrid.SelectedRows.Count > 0)
+            //{
+                //MessageBox.Show();
+            //}
         }
     }
 }
